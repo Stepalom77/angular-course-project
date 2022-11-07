518a90dd2e7ae647c476625c01173f58c1edd2bc
Stepalom77 <stepalom22@gmail.com>
2022-11-07 10:24:17 -0500

    recipe model and recipe-list component

--- src/app/recipe_book/recipe-list/recipe-list.component.ts
+++ src/app/recipe_book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes/recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes:Recipe[] = [
+    new Recipe('Arroz con pollo', 'This is a recipe for the arroz con pollo peruvian dish',
+     'https://comidasperuanas.net/wp-content/uploads/2015/07/Arroz-con-pollo-peruano.webp')
+  ]
   constructor() { }
 
   ngOnInit(): void {
