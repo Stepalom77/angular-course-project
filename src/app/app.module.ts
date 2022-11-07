fd4719f0aed4e4d057b900f0f545d1796a84c2d4
Stepalom77 <stepalom22@gmail.com>
2022-11-07 09:23:54 -0500

    project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './recipe_book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe_book/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe_book/recipe-detail/recipe-detail.component';
+import { RecipesComponent } from './recipe_book/recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule
