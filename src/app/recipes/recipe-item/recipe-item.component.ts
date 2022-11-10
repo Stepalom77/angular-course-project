ce7bb7f52b86f48ccd0689850be972da1dd21009
Stepalom77 <stepalom22@gmail.com>
2022-11-10 15:46:17 -0500

    recipes-start component added and routes added to recipes

--- src/app/recipes/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-item/recipe-item.component.ts
@@ -9,14 +9,10 @@ import { Recipe } from '../recipes.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe:Recipe
+  @Input() index:number
 
   constructor(private recipeService:RecipeService) { }
 
   ngOnInit(): void {
   }
-
-  onSelected(){
-    this.recipeService.recipeSelected.emit(this.recipe)
-  }
-
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -12,12 +12,20 @@ export class RecipeService {
     new Recipe('Arroz con pollo', 'This is a recipe for the arroz con pollo peruvian dish',
      'https://comidasperuanas.net/wp-content/uploads/2015/07/Arroz-con-pollo-peruano.webp', [
       new Ingredient('Rice', 2), new Ingredient('Chicken', 1)
+     ]),
+     new Recipe('Arroz con pollo', 'This is a recipe for the arroz con pollo peruvian dish',
+     'https://comidasperuanas.net/wp-content/uploads/2015/07/Arroz-con-pollo-peruano.webp', [
+      new Ingredient('Rice', 2), new Ingredient('Chicken', 1)
      ])
   ]
   constructor(private shoppingListService:ShoppingListService) {}
 
   getRecipes(){
     return this.recipes.slice()
+  }
+
+  getRecipe(index:number){
+    return this.recipes[index]
   }
 
   addIngredientsToShoppingList(ingredients:Ingredient[]){
