17b5c32a98bd643a37cd84644916cf19905f481c
Stepalom77 <stepalom22@gmail.com>
2022-11-12 16:00:22 -0500

    Adding Auth Service and signup method

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+  isLoginMode = true
 
-  constructor() { }
+  constructor(private authService:AuthService) {}
 
   ngOnInit(): void {
   }
 
+  onSwitchMode(){
+    this.isLoginMode = !this.isLoginMode
+  }
+
+  onSubmit(form:NgForm){
+    if(!form.value){
+      return
+    }
+    const email = form.value.email
+    const password = form.value.password
+
+    if(this.isLoginMode){
+
+    } else {
+      this.authService.signup(email, password).subscribe(
+        resData => {
+          console.log(resData)
+        },
+        error => {
+          console.log(error)
+        }
+
+      )
+    }
+    form.reset()
+  }
 }
