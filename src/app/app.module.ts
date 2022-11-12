7cc7fb0f5451856c612a1ee6b6731374992a9efe
Stepalom77 <stepalom22@gmail.com>
2022-11-12 16:26:43 -0500

    spinner and error handling added to auth component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import {HttpClientModule} from '@angular/common/http';
-import { AuthComponent } from './auth/auth.component'
+import { AuthComponent } from './auth/auth.component';
+import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
+
 
 @NgModule({
   declarations: [
@@ -29,7 +31,8 @@ import { AuthComponent } from './auth/auth.component'
     DropdownDirective,
     RecipeStartComponent,
     RecipeEditComponent,
-    AuthComponent
+    AuthComponent,
+    LoadingSpinnerComponent
   ],
   imports: [
     BrowserModule, FormsModule, AppRoutingModule, ReactiveFormsModule, HttpClientModule
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -9,6 +9,8 @@ import { AuthService } from './auth.service';
 })
 export class AuthComponent implements OnInit {
   isLoginMode = true
+  isLoading = false
+  error:string = null
 
   constructor(private authService:AuthService) {}
 
@@ -27,14 +29,17 @@ export class AuthComponent implements OnInit {
     const password = form.value.password
 
     if(this.isLoginMode){
-
+      this.isLoading = true
     } else {
       this.authService.signup(email, password).subscribe(
         resData => {
           console.log(resData)
+          this.isLoading = false
         },
-        error => {
-          console.log(error)
+        errorMessage => {
+          console.log(errorMessage)
+          this.error = errorMessage
+          this.isLoading = false
         }
 
       )
