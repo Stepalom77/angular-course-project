7688c542c96d9ab476995caf287149ff4ca004e8
Stepalom77 <stepalom22@gmail.com>
2022-11-12 17:08:35 -0500

    login method added

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,13 +1,14 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   idToken:string;
   email:string;
   refreshToken:string;
   expiresIn:string;
-  localId:string
+  localId:string;
+  registered?:boolean
 }
 
 @Injectable({
@@ -22,16 +23,32 @@ export class AuthService {
       email:email,
       password:password,
       returnSecureToken:true
-    }).pipe(catchError(errorRes => {
-      let errorMessage = 'An unknown error ocurred!'
-      if(!errorRes.error || !errorRes.error.error){
-        return throwError(errorMessage)
-      }
-      switch(errorRes.error.error.message){
-        case 'EMAIL_EXISTS' :
-        errorMessage = 'This email exists already!!'
-      }
+    }).pipe(catchError(this.handleError))
+  }
+
+  login(email:string, password:string){
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAhaeZwM7ZRdYhWPrmzBN4WBe5ynoQS6uI', {
+      email:email,
+      password:password,
+      returnSecureToken:true
+    }).pipe(catchError(this.handleError))
+  }
+
+  private handleError(errorRes:HttpErrorResponse){
+    let errorMessage = 'An unknown error ocurred!'
+    if(!errorRes.error || !errorRes.error.error){
       return throwError(errorMessage)
-    }))
+    }
+    switch(errorRes.error.error.message){
+      case 'EMAIL_EXISTS' :
+        errorMessage = 'This email exists already!!';
+      break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email does not exists';
+      break;
+      case 'USER_DISABLED' :
+        errorMessage = 'This user account has been disabled'
+    }
+    return throwError(errorMessage)
   }
 }
